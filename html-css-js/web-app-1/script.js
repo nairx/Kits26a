f4f46b3e5c1805deb58de2442c04c368cdde5aac
nairx <nairxprv@gmail.com>
2026-01-10 11:51:03 +0530

    updated

--- html-css-js/web-app-1/script.js
+++ html-css-js/web-app-1/script.js
@@ -1,8 +1,15 @@
-function dispMessage(){
-    // a=10
-    // b=20
-    // c=a+b
-    //alert("Hello")
-    // alert(document.getElementById("txtEmail").value)
-    lblMessage.innerHTML = document.getElementById("txtEmail").value
+function dispMessage() {
+  // a=10
+  // b=20
+  // c=a+b
+  //alert("Hello")
+  // alert(document.getElementById("txtEmail").value)
+  // lblMessage.innerHTML = document.getElementById("txtEmail").value + "-" + document.getElementById("txtPassword").value
+  let email = document.getElementById("txtEmail").value;
+  let password = document.getElementById("txtPassword").value
+  if (email === "john@gmail.com" && password === "1234") {
+    lblMessage.innerHTML = "Welcome";
+  } else {
+    lblMessage.innerHTML = "Access Denied";
+  }
 }
